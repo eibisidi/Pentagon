2bd5f4dc83f36cb8493b5122ec8bdc41b15c6f62
eibisidi <5555735+eibisidi@user.noreply.gitee.com>
2024-12-18 15:09:07 +0800

    add scripts

--- matlab/fiveLinkageDemo.m
+++ matlab/fiveLinkageDemo.m
@@ -29,7 +29,7 @@ UIAxes = axes(UIAxesFigure);
 title(UIAxes, '5 linkage');
 xlabel(UIAxes, 'X');
 ylabel(UIAxes, 'Y');
-set(UIAxes, 'ButtonDownFcn', @MouseClicked);
+set(UIAxes, 'ButtonDownFcn', @AxesMouseClicked);
 
 % Create theta1SliderLabel
 theta1SliderLabel = uilabel(UIFigure);
@@ -90,7 +90,15 @@ downModeLabel.FontSize = 16;
 downModeLabel.Position = [211 532 82 22];
 downModeLabel.Text = 'downMode';
 
-loci(UIAxes, fiveLinkage.r); %draw singularity curve
+ %draw singularity curve
+[fp_C1o, fp_C1i, fp_C2o, fp_C2i, fp_CCoin_U, fp_CCoin_D, fp_CCol] = loci(UIAxes, fiveLinkage.r);
+fp_C1o.PickableParts = 'none';
+fp_C1i.PickableParts = 'none';
+fp_C2o.PickableParts = 'none';
+fp_C2i.PickableParts = 'none';
+fp_CCoin_U.PickableParts = 'none';
+fp_CCoin_D.PickableParts = 'none';
+fp_CCol.PickableParts = 'none';
 handleThetaChanged();
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
@@ -110,8 +118,9 @@ function theta2SliderValueChanged(~, event)
     handleThetaChanged();
 end
 
-function [outputArg1,outputArg2] = MouseClicked(Object, EventData)
-disp(Object.CurrentPoint);
+function [outputArg1,outputArg2] = AxesMouseClicked(Object, eventData)
+ cp = Object.CurrentPoint(1,:);
+ disp(cp);
 end
 
 function results = handleThetaChanged()
@@ -172,7 +181,9 @@ function results = drawActiveLink()
         delete(h_a2b2);
     end
     h_a1b1 = plot(UIAxes, [fiveLinkage.A1(1) fiveLinkage.b1(1)] , [fiveLinkage.A1(2) fiveLinkage.b1(2)], '-go');
+    h_a1b1.PickableParts = 'none';
     h_a2b2 = plot(UIAxes, [fiveLinkage.A2(1) fiveLinkage.b2(1)] , [fiveLinkage.A2(2) fiveLinkage.b2(2)], '-go');
+    h_a2b2.PickableParts = 'none';
     results = 0;
 end
         
@@ -204,9 +215,11 @@ function results = drawPassiveLink()
     end
 
     h_b1c1b2 = plot(UIAxes, [fiveLinkage.b1(1) fiveLinkage.fk_up(1) fiveLinkage.b2(1)] , [fiveLinkage.b1(2) fiveLinkage.fk_up(2) fiveLinkage.b2(2)], upLineSpec);
-
+    h_b1c1b2.PickableParts = 'none';
+    
     if (fiveLinkage.fk_nSol == 2)
         h_b1c2b2 = plot(UIAxes, [fiveLinkage.b1(1) fiveLinkage.fk_down(1) fiveLinkage.b2(1)] , [fiveLinkage.b1(2) fiveLinkage.fk_down(2) fiveLinkage.b2(2)], downLineSpec);
+        h_b1c2b2.PickableParts = 'none';
     end
 
     results = 0;                    
--- matlab/loci.m
+++ matlab/loci.m
@@ -1,4 +1,4 @@
-function [] = loci(ax, rs)
+function [fp_C1o, fp_C1i, fp_C2o, fp_C2i, fp_CCoin_U, fp_CCoin_D, fp_CCol] = loci(ax, rs)
 %loci Draw all singularity points curve
 %   ax: axis to plot into
 %   rs: [r1; r2; r3] vector
@@ -18,24 +18,24 @@ C1o = @(x,y) (x + r3).^2 + y.^2 - (r1 + r2)^2;   %Equation (13)
 C1i = @(x,y) (x + r3).^2 + y.^2 - (r1 - r2)^2;   %Equation (14)
 C2o = @(x,y) (x - r3).^2 + y.^2 - (r1 + r2)^2;   %Equation (15)
 C2i = @(x,y) (x - r3).^2 + y.^2 - (r1 - r2)^2;   %Equation (16)
-fimplicit(ax, C1o, '-b')
-fimplicit(ax, C1i, '-b')
-fimplicit(ax, C2o, '-b')
-fimplicit(ax, C2i, '-b')
+fp_C1o = fimplicit(ax, C1o, '-b');
+fp_C1i = fimplicit(ax, C1i, '-b');
+fp_C2o = fimplicit(ax, C2o, '-b');
+fp_C2i = fimplicit(ax, C2i, '-b');
 
 
 %Uncertainty Singularity
 %Type a Equation (25) Fig.6
 CCoin_U = @(x,y) x.*x + (y-sqrt(r1*r1 - r3*r3)).^2 - r2 * r2;
-fimplicit(ax, CCoin_U, '-k')
+fp_CCoin_U = fimplicit(ax, CCoin_U, '-k');
 
 CCoin_D = @(x,y) x.*x + (y+sqrt(r1*r1 - r3*r3)).^2 - r2 * r2;
-fimplicit(ax, CCoin_D, '-k')
+fp_CCoin_D = fimplicit(ax, CCoin_D, '-k');
 
 %Type b Equation (28) Fig.7
 % fiib = @(x,y) ((x + 2 * r3).^2) .* ((x.^2 + 4 * r3 * x + 3 * r3* r3 + r2 * r2 - r1 * r1 + y.^2).^2) + (y.^2).*((x.^2 + 4 * r3*x + 5 * r3*r3 + r2*r2 - r1*r1 + y.^2).^2) - 4*r3*r3*r2*r2*(y.^2);
 CCol = @(x,y) ((x).^2) .* ((x.^2 - r3 * r3 + r2 * r2 - r1 * r1 + y.^2).^2) + (y.^2).*((x.^2 + r3 * r3 + r2 * r2 - r1*r1 + y.^2).^2) - 4*r3*r3*r2*r2*(y.^2);
-fimplicit(ax, CCol, '-r')
+fp_CCol = fimplicit(ax, CCol, '-r');
 
 end
 
