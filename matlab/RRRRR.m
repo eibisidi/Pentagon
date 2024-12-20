1d32519354a67c18a8169278f01f1b1e89013872
eibisidi <5555735+eibisidi@user.noreply.gitee.com>
2024-12-20 13:10:46 +0800

    redesign RRRRR class

--- matlab/RRRRR.m
+++ matlab/RRRRR.m
@@ -1,25 +1,57 @@
 classdef RRRRR
     %basic 5 bar linkage class
+    properties( Constant = true )
+        CONF_NONE = -1
+        CONF_UP   = 1
+        CONF_DOWN = 2
+        MODE_NONE = -1
+        MODE_PP   = 1
+        MODE_PN   = 2
+        MODE_NP   = 3
+        MODE_NN   = 4
+    end
     
     properties (Access = public)
+        %Fixed values
         r % linkage length [r1;r2;r3]
-        theta %active joint degree with x axis [theta1;theta2]
         A1 %A1 coordinate [-r3; 0]
         A2 %A2 coordinate [r3; 0]
+        
+        %Current values
+        theta %active joint degree with x axis [theta1;theta2]
         B1 %B1 coordinate dependent on theta1
         B2 %B2 coordinate dependent on theta2
-        fk_nSol;  %forward kinematics solution number;
-        fk_up;    %up-configuration coordinate[x;y]
-        fk_down;  %down-configuration coordinate[x;y]
-        fk_up_theta; %up-configuration PB1&PB2 degree with axis x[theta3; theta4]
-        fk_down_theta; %down-configuration PB1&PB2 degree with axis x[theta3; theta4]
+        passive_theta %active joint degree with x axis [theta3;theta4]
+        P  %end point coordinate[x; y]
         current_configuration;     %-1 no solution; 0 down; 1 up
         initial_configuration;     % 0 down; 1 up
-        current_position;          % end-point coordinate [x; y]
-        ik_pp; % ++mode [theta1; theta2]
-        ik_pn; % +-mode [theta1; theta2]
-        ik_np; % -+mode [theta1; theta2]
-        ik_nn; % --mode [theta1; theta2]
+        
+        %FK results
+        fk_B1;
+        fk_B2;
+        fk_nSol;  %forward kinematics solution number; -1 Inf, 0 no, 1/2
+        fk_P;     %forward  [up down]
+        fk_modes; %work mode [up down]
+        fk_passive_theta; %passive joint thetas [up down]
+        
+        %IF results
+        ik_nSol;    %0/4
+        ik_theta;   %active joint theta [pp pn np nn]
+        ik_B1;
+        ik_B2;
+        ik_passive_theta;  %passvie joint theta [pp pn np nn]
+        ik_configuration;  %configuration [pp pn np nn]
+        %         fk_up;    %up-configuration coordinate[x;y]
+        %         fk_down;  %down-configuration coordinate[x;y]
+        %         fk_up_theta; %up-configuration PB1&PB2 degree with axis x[theta3; theta4]
+        %         fk_down_theta; %down-configuration PB1&PB2 degree with axis x[theta3; theta4]
+        %         current_configuration;     %-1 no solution; 0 down; 1 up
+        %         initial_configuration;     % 0 down; 1 up
+        %         current_position;          % end-point coordinate [x; y]
+        %         ik_pp; % ++mode [theta1; theta2]
+        %         ik_pn; % +-mode [theta1; theta2]
+        %         ik_np; % -+mode [theta1; theta2]
+        %         ik_nn; % --mode [theta1; theta2]
     end
     
     methods
@@ -33,6 +65,38 @@ classdef RRRRR
             end
         end
         
+        function mode = string2mode(fiveLinkage, string)
+            switch string
+                case '++'
+                    mode = fiveLinkage.MODE_PP;
+                case '+-'
+                    mode = fiveLinkage.MODE_PN;
+                case '-+'
+                    mode = fiveLinkage.MODE_NP;
+                case '--'
+                    mode = fiveLinkage.MODE_NN;
+                otherwise
+                    mode = fiveLinkage.MODE_NONE;
+            end
+        end
+        
+        function modeStr = getWorkModeString(fiveLinkage, configuration)
+            mode = fiveLinkage.getWorkMode(configuration);
+            switch mode
+                case fiveLinkage.MODE_PP
+                    modeStr = '++';
+                case fiveLinkage.MODE_PN
+                    modeStr = '+-';
+                case fiveLinkage.MODE_NP
+                    modeStr = '-+';
+                case fiveLinkage.MODE_NN
+                    modeStr = '--';
+                otherwise
+                    modeStr = 'xx';
+            end
+            return;
+        end
+        
         function mode = getWorkMode(fiveLinkage, configuration)
             %After forward kinematics, get work mode
             %input argument:
@@ -43,24 +107,12 @@ classdef RRRRR
             %           '++' '+-' '-+' '--' corresponding work mode of
             %           specified configuration
             if (fiveLinkage.fk_nSol < 1)
-                mode = 'xx';
+                mode = fiveLinkage.MODE_NONE;
                 return;
             end
-            
-            p = fiveLinkage.fk_up;
-            if (configuration ~= 1)
-                p = fiveLinkage.fk_down;
-            end
-            
-            A1C1 = p - fiveLinkage.A1; %vector A1P
-            A1B1 = fiveLinkage.B1 - fiveLinkage.A1;    %vector A1B1
-            leftCross = A1C1(1) * A1B1(2) - A1B1(1) * A1C1(2);
-            
-            A2C1 = p  - fiveLinkage.A2;
-            A2B2 = fiveLinkage.B2 - fiveLinkage.A2;
-            rightCross = A2C1(1) * A2B2(2) - A2B2(1) * A2C1(2);
-            
-            mode = [fiveLinkage.cross2char(leftCross) , fiveLinkage.cross2char(rightCross)];
+
+            p =  fiveLinkage.fk_P(:, configuration);
+            mode = fiveLinkage.calcWorkMode(fiveLinkage.fk_B1, fiveLinkage.fk_B2, p);
             return;
         end
         
@@ -111,7 +163,53 @@ classdef RRRRR
             return;
         end
         
-        function [b1, b2, nSol, up, down, up_theta, down_theta] = forwardKinematics(fiveLinkage)
+        function mode = calcWorkMode(fiveLinkage, b1, b2, p)
+            A1C1 = p - fiveLinkage.A1;      %vector A1P
+            A1B1 = b1 - fiveLinkage.A1;     %vector A1B1
+            leftCross = A1C1(1) * A1B1(2) - A1B1(1) * A1C1(2);
+            
+            A2C1 = p  - fiveLinkage.A2;
+            A2B2 = b2 - fiveLinkage.A2;
+            rightCross = A2C1(1) * A2B2(2) - A2B2(1) * A2C1(2);
+            
+            if (leftCross >= -1E-6  && rightCross >= -1E-6)
+                mode = fiveLinkage.MODE_PP;
+            elseif (leftCross >= -1E-6 && rightCross < -1E-6)
+                mode = fiveLinkage.MODE_PN;
+            elseif (leftCross < -1E-6 && rightCross >= -1E-6)
+                mode = fiveLinkage.MODE_NP;
+            else
+                mode = fiveLinkage.MODE_NN;
+            end
+            return;
+        end
+        
+        function [b1, b2] = calcB1B2(fiveLinkage, joint_theta)
+            s1 = sind(joint_theta(1));
+            c1 = cosd(joint_theta(1));
+            s2 = sind(joint_theta(2));
+            c2 = cosd(joint_theta(2));
+            b1 = [fiveLinkage.r(1) * c1 - fiveLinkage.r(3); fiveLinkage.r(1) * s1];
+            b2 = [fiveLinkage.r(1) * c2 + fiveLinkage.r(3); fiveLinkage.r(1) * s2];
+        end
+        
+        function passive_theta = calcPassiveTheta(~, b1, b2, p)
+            u = p - b1;
+            v = p - b2;
+            passive_theta = [atan2d(u(2), u(1)); atan2d(v(2), v(1))];
+        end
+        
+        function configuration = calcConfiguration(fiveLinkage, b1, b2, p)
+            midY = (b1(2) + b2(2)) / 2;
+            
+            if (p(2) >= midY)
+                configuration = fiveLinkage.CONF_UP;%up
+            else
+                configuration = fiveLinkage.CONF_DOWN; %down
+            end
+        end
+        
+        function [fiveLinkage] = forwardKinematics(fiveLinkage, newTheta)
             %forward kinematics
             %input arguments:
             %               r: [r1; r2; r3] column vector
@@ -128,22 +226,23 @@ classdef RRRRR
             %           down: down-configuration end point coordinate [x;y]
             %           up_theta: up-configuration [theta3; theta4]
             %           down_theta: down-configuration [theta3; theta4]
-            s1 = sind(fiveLinkage.theta(1));
-            c1 = cosd(fiveLinkage.theta(1));
-            s2 = sind(fiveLinkage.theta(2));
-            c2 = cosd(fiveLinkage.theta(2));
+            s1 = sind(newTheta(1));
+            c1 = cosd(newTheta(1));
+            s2 = sind(newTheta(2));
+            c2 = cosd(newTheta(2));
             
             b1 = [fiveLinkage.r(1) * c1 - fiveLinkage.r(3); fiveLinkage.r(1) * s1];
             b2 = [fiveLinkage.r(1) * c2 + fiveLinkage.r(3); fiveLinkage.r(1) * s2];
+            fiveLinkage.fk_B1 = b1;
+            fiveLinkage.fk_B2 = b2;
             
             denominator =  (2 * fiveLinkage.r(3) + fiveLinkage.r(1) * c2 - fiveLinkage.r(1) * c1);
             if (abs(denominator) < 1E-6)
                 %infinitely many solutions
-                nSol = -1;
-                up = [];
-                down = [];
-                up_theta = [];
-                down_theta = [];
+                fiveLinkage.fk_nSol = -1;
+                fiveLinkage.fk_P = [];
+                fiveLinkage.fk_modes = [];
+                fiveLinkage.fk_passive_theta = [];
                 return;
             end
             
@@ -157,24 +256,27 @@ classdef RRRRR
             
             if (det < -1E-6)
                 %no solution
-                nSol = 0;
-                up = [];
-                down = [];
-                up_theta = [];
-                down_theta = [];
+                fiveLinkage.fk_nSol = 0;
+                fiveLinkage.fk_P = [];
+                fiveLinkage.fk_modes = [];
+                fiveLinkage.fk_passive_theta = [];
                 return;
             end
             
             if (det < 1E-6)
                 y = -g / 2 / d;
                 x = e * y + f;
-                nSol = 1;
                 up = [x; y];
-                down = [x; y];
                 u = up - b1;
                 v = up - b2;
+                up_mode = calcWorkMode(fiveLinkage, b1, b2, up);
                 up_theta = [atan2d(u(2), u(1)); atan2d(v(2), v(1))];
-                down_theta = up_theta;
+                
+                fiveLinkage.fk_nSol = 1;
+                fiveLinkage.fk_P = [up up];
+                fiveLinkage.fk_modes = [up_mode up_mode];
+                fiveLinkage.fk_passive_theta = [up_theta up_theta];
+                
                 return;
             end
             
@@ -184,18 +286,24 @@ classdef RRRRR
             y2 = (-g - sqrt(det)) / 2 /d;
             x2 = e * y2 + f;
             
-            nSol = 2;
             up = [x1; y1];
             down = [x2; y2];
+            up_mode = calcWorkMode(fiveLinkage, b1, b2, up);
+            down_mode = calcWorkMode(fiveLinkage, b1, b2, down);
             u = up - b1;
             v = up - b2;
             up_theta = [atan2d(u(2), u(1)); atan2d(v(2), v(1))];
             u = down - b1;
             v = down - b2;
             down_theta = [atan2d(u(2), u(1)); atan2d(v(2), v(1))];
+            
+            fiveLinkage.fk_nSol = 2;
+            fiveLinkage.fk_P = [up down];
+            fiveLinkage.fk_modes = [up_mode down_mode];
+            fiveLinkage.fk_passive_theta = [up_theta down_theta];
         end
         
-        function [pp, pn, np, nn] = inverseKinematics(fiveLinkage, p)
+        function [fiveLinkage] = inverseKinematics(fiveLinkage, newP)
             %inverse kinematics
             %input arguments:
             %               fiveLinkage: RRRRR object
@@ -208,8 +316,8 @@ classdef RRRRR
             r1 = fiveLinkage.r(1);
             r2 = fiveLinkage.r(2);
             r3 = fiveLinkage.r(3);
-            x = p(1);
-            y = p(2);
+            x = newP(1);
+            y = newP(2);
             
             a1 = r1^2 + y^2 + (x + r3)^2 - r2^2 + 2 * (x + r3) * r1;
             b1 = -4 * y * r1;
@@ -221,10 +329,12 @@ classdef RRRRR
             det1 = b1^2 - 4 * a1 * c1;
             det2 = b2^2 - 4 * a2 * c2;
             if ( det1 < -1E-6 || det2 < -1E-6)
-                pp = [];
-                pn = [];
-                np = [];
-                nn = [];
+                fiveLinkage.ik_nSol = 0;
+                fiveLinkage.ik_theta = [];
+                fiveLinkage.ik_B1 = [];
+                fiveLinkage.ik_B2 = [];
+                fiveLinkage.ik_passive_theta = [];
+                fiveLinkage.ik_configuration = [];
                 return;
             end
             
@@ -269,8 +379,87 @@ classdef RRRRR
             pn(2) = theta2;
             nn(2) = theta2;
             
+            [pp_b1, pp_b2] = calcB1B2(fiveLinkage, pp);
+            [pn_b1, pn_b2] = calcB1B2(fiveLinkage, pn);
+            [np_b1, np_b2] = calcB1B2(fiveLinkage, np);
+            [nn_b1, nn_b2] = calcB1B2(fiveLinkage, nn);
+            
+            pp_passive_theta = calcPassiveTheta(fiveLinkage, pp_b1, pp_b2, newP);
+            pn_passive_theta = calcPassiveTheta(fiveLinkage, pn_b1, pn_b2, newP);
+            np_passive_theta = calcPassiveTheta(fiveLinkage, np_b1, np_b2, newP);
+            nn_passive_theta = calcPassiveTheta(fiveLinkage, nn_b1, nn_b2, newP);
+            
+            pp_conf = calcConfiguration(fiveLinkage, pp_b1, pp_b2, newP);
+            pn_conf = calcConfiguration(fiveLinkage, pn_b1, pn_b2, newP);
+            np_conf = calcConfiguration(fiveLinkage, np_b1, np_b2, newP);
+            nn_conf = calcConfiguration(fiveLinkage, nn_b1, nn_b2, newP);
+            
+            fiveLinkage.ik_nSol = 4;
+            fiveLinkage.ik_theta = [pp pn np nn];
+            fiveLinkage.ik_B1 = [pp_b1 pn_b1 np_b1 nn_b1];
+            fiveLinkage.ik_B2 = [pp_b2 pn_b2 np_b2 nn_b2];
+            fiveLinkage.ik_passive_theta = [pp_passive_theta pn_passive_theta np_passive_theta nn_passive_theta];
+            fiveLinkage.ik_configuration = [pp_conf pn_conf np_conf nn_conf];
             return;
         end
         
+        function fiveLinkage = setTheta(fiveLinkage, newTheta)
+            fiveLinkage = fiveLinkage.forwardKinematics(newTheta);
+            fiveLinkage.theta = newTheta;
+            fiveLinkage.B1 = fiveLinkage.fk_B1;
+            fiveLinkage.B2 = fiveLinkage.fk_B2;
+            
+            if (fiveLinkage.fk_nSol < 1)
+                %no solution / INF solutions
+                fiveLinkage.passive_theta = [];
+                fiveLinkage.P = [];
+                fiveLinkage.current_configuration = fiveLinkage.CONF_NONE;
+                return;
+            end
+            
+            %there is solutions
+            if (fiveLinkage.current_configuration < 0)
+                fiveLinkage.current_configuration = fiveLinkage.initial_configuration;
+            else
+                diff_up   = fiveLinkage.fk_P(:,fiveLinkage.CONF_UP) - fiveLinkage.P;
+                diff_down = fiveLinkage.fk_P(:,fiveLinkage.CONF_DOWN) - fiveLinkage.P;
+                if (norm(diff_up) < norm(diff_down))
+                    fiveLinkage.current_configuration = fiveLinkage.CONF_UP;
+                else
+                    fiveLinkage.current_configuration = fiveLinkage.CONF_DOWN;
+                end
+            end
+            
+            fiveLinkage.P = fiveLinkage.fk_P(:,fiveLinkage.current_configuration);
+            fiveLinkage.passive_theta = fiveLinkage.fk_passive_theta(:,fiveLinkage.current_configuration);
+        end
+        
+        function valid = isValid(fiveLinkage)
+            valid = 1;
+            if (fiveLinkage.current_configuration == fiveLinkage.CONF_NONE)
+                valid = 0;
+            end
+            return;
+        end
+        
+        function fiveLinkage = setP(fiveLinkage, newP, modeString)
+            fiveLinkage = fiveLinkage.inverseKinematics(newP);
+            if (fiveLinkage.ik_nSol < 4)
+                return;
+            end
+            
+            %IK success, update current status
+            mode = fiveLinkage.string2mode(modeString);
+            
+            fiveLinkage.theta = fiveLinkage.ik_theta(:, mode);
+            fiveLinkage.B1 = fiveLinkage.ik_B1(:, mode);
+            fiveLinkage.B2 = fiveLinkage.ik_B2(:, mode);
+            
+            fiveLinkage.passive_theta = fiveLinkage.ik_passive_theta(:, mode);
+            fiveLinkage.P = newP;
+            fiveLinkage.current_configuration = fiveLinkage.ik_configuration(:, mode);
+            
+            return;
+        end
     end
 end
--- matlab/fiveLinkageDemo.m
+++ matlab/fiveLinkageDemo.m
@@ -8,14 +8,18 @@ fiveLinkage.r = [1.2; 1.0; 0.8];
 fiveLinkage.theta = [0; 0];
 fiveLinkage.A1 = [-fiveLinkage.r(3); 0];
 fiveLinkage.A2 = [fiveLinkage.r(3); 0];
-fiveLinkage.current_configuration = -1;
-fiveLinkage.initial_configuration = 1;  %use up configuration when startup
+fiveLinkage.current_configuration = fiveLinkage.CONF_NONE;
+fiveLinkage.initial_configuration = fiveLinkage.CONF_UP;  %use up configuration when startup
 
 %Rotation axis
-global L phi tooltip;
+global L phi tooltip thetaR;
 L = 0.2;    %link length
-phi = 0;    %direction angle
-tooltip = [];
+phi = 0;    %direction angle wrt. x axis
+tooltip = []; %tooltip coordinate [x;y]
+thetaR = 0;      %R joint roation angle
+
+global steps;
+steps = 0;
 
 global h_a1b1 h_a2b2 h_b1c1b2 h_b1c2b2 h_end_effector;
 h_a1b1 = []; %matlab plot handle of active link A1B1
@@ -141,21 +145,21 @@ fp_C2i.PickableParts = 'none';
 fp_CCoin_U.PickableParts = 'none';
 fp_CCoin_D.PickableParts = 'none';
 fp_CCol.PickableParts = 'none';
-handleThetaChanged();
+handleThetaChanged(theta1Slider.Value, theta2Slider.Value, thetaR);
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %UI Callbacks
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 function theta1SliderValueChanged(~, event)
-    global fiveLinkage;
-    fiveLinkage.theta(1) = event.Value;
-    handleThetaChanged();
+    global theta2Slider;
+    global thetaR;
+    handleThetaChanged(event.Value, theta2Slider.Value, thetaR);
 end
 
 function theta2SliderValueChanged(~, event)
-    global fiveLinkage;
-    fiveLinkage.theta(2) = event.Value;
-    handleThetaChanged();
+    global theta1Slider;
+    global thetaR;
+    handleThetaChanged(theta1Slider.Value, event.Value, thetaR);
 end
 
 function phiSliderValueChanged(~, event)
@@ -167,36 +171,27 @@ end
 
 function  AxesMouseClicked(Object, ~)
     global UIModeKnob;
+    global steps;
     cp = Object.CurrentPoint(1,:);
-    p = [cp(1) ; cp(2)];
-    handleEndPointChanged(p, UIModeKnob.Value);
-end
-
-function KnobValueChanged(~, event)
-    global tooltip;
-    handleEndPointChanged(tooltip, event.Value);
-end
-
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-%Functions
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-function handleEndPointChanged(tooltip, mode)
-    %Input argument
-    %   tooltip: end-effector coordinate [x; y]
-    %   mode: work mode requireds
-    global fiveLinkage;
-    global theta1Slider theta2Slider;
-    global L phi;
-    p = tooltip;
-    if ( L > 0)
-        p(1) = tooltip(1) - L * cosd(phi);
-        p(2) = tooltip(2) - L * sind(phi);
+    endEffector = [cp(1) ; cp(2)];
+    if (steps == 0)
+        handleEndPointChanged(endEffector, UIModeKnob.Value);
+        return;
     end
     
+    global fiveLinkage;
+    global L phi R;
+    startJoint = [fiveLinkage.theta(1); fiveLinkage.theta(2); R];
+    
+    p = endEffector;
+    if ( L > 0)
+        p(1) = endEffector(1) - L * cosd(phi);
+        p(2) = endEffector(2) - L * sind(phi);
+    end
     [fiveLinkage.ik_pp, fiveLinkage.ik_pn, fiveLinkage.ik_np, fiveLinkage.ik_nn] = fiveLinkage.inverseKinematics(p);
     %disp([fiveLinkage.ik_pp, fiveLinkage.ik_pn, fiveLinkage.ik_np, fiveLinkage.ik_nn] );
 
-    switch mode
+    switch  UIModeKnob.Value
     case '++'
         solution = fiveLinkage.ik_pp;
     case '+-'
@@ -211,55 +206,81 @@ function handleEndPointChanged(tooltip, mode)
         return;
     end
     
-    theta1Slider.Value = solution(1);
-    theta2Slider.Value = solution(2);
-    fiveLinkage.current_configuration = -1;
     fiveLinkage.current_position = p;
-    fiveLinkage.initial_configuration = fiveLinkage.getConfiguration(mode);
-    fiveLinkage.theta = solution;
-    handleThetaChanged();
+    fiveLinkage.current_configuration = fiveLinkage.getConfiguration( UIModeKnob.Value);
+	fiveLinkage.theta = solution;
+    [fiveLinkage.B1,fiveLinkage.B2, fiveLinkage.fk_nSol, fiveLinkage.fk_up, fiveLinkage.fk_down, fiveLinkage.fk_up_theta, fiveLinkage.fk_down_theta] = fiveLinkage.forwardKinematics();
+    
+    if (fiveLinkage.current_configuration == 1)
+        rotation = phi - fiveLinkage.fk_up_theta(1);
+    else
+        rotation = phi - fiveLinkage.fk_down_theta(1);
+    end
+    endJoint = [solution(1);solution(2); rotation];
+    
+    distance = endJoint - startJoint;
+    for i = 1:1:steps
+        disp(i);
+        ratio = i / steps;
+        t1 = startJoint(1) + ratio * distance(1);
+        t2 = startJoint(2) + ratio * distance(2);
+        fiveLinkage.theta = [t1; t2];
+        handleThetaChanged();
+        pause(0.1);
+    end
 end
 
-function handleThetaChanged()
+function KnobValueChanged(~, event)
+    global tooltip;
+    handleEndPointChanged(tooltip, event.Value);
+end
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%Functions
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+function handleEndPointChanged(newToolTip, mode)
+    %Input argument
+    %   tooltip: end-effector coordinate [x; y]
+    %   mode: work mode requireds
     global fiveLinkage;
-
-    %do forward kinematics
-    [fiveLinkage.B1,fiveLinkage.B2, fiveLinkage.fk_nSol, fiveLinkage.fk_up, fiveLinkage.fk_down, fiveLinkage.fk_up_theta, fiveLinkage.fk_down_theta] = fiveLinkage.forwardKinematics();
-    drawActiveLink();
+    global theta1Slider theta2Slider;
+    global L phi thetaR;
+    p = newToolTip;
+    if ( L > 0)
+        p(1) = newToolTip(1) - L * cosd(phi);
+        p(2) = newToolTip(2) - L * sind(phi);
+    end
     
-    if (fiveLinkage.fk_nSol < 1)
-        %no solution
-        fiveLinkage.current_configuration = -1;
-        fiveLinkage.current_position = [];
-    elseif (fiveLinkage.current_configuration < 0)
-        fiveLinkage.current_configuration = fiveLinkage.initial_configuration;
-    else
-        diff_up   = fiveLinkage.fk_up - fiveLinkage.current_position;
-        diff_down = fiveLinkage.fk_down - fiveLinkage.current_position;
-        if (norm(diff_up) < norm(diff_down))
-            fiveLinkage.current_configuration = 1;
-        else
-            fiveLinkage.current_configuration = 0;
-        end
-    end
+    fiveLinkage = fiveLinkage.setP(p, mode);
+    
+    theta1Slider.Value = fiveLinkage.theta(1);
+    theta2Slider.Value = fiveLinkage.theta(2);
+    handleThetaChanged(fiveLinkage.theta(1), fiveLinkage.theta(2), thetaR);
+end
 
-    if (fiveLinkage.current_configuration == 1)
-        fiveLinkage.current_position = fiveLinkage.fk_up;
-    else
-        fiveLinkage.current_position = fiveLinkage.fk_down;
-    end
-
-    %update tooltip coordinate
+function handleThetaChanged(theta1, theta2, thetaR)
+    global fiveLinkage;
+    fiveLinkage = fiveLinkage.setTheta([theta1; theta2]);
+    
     global L phi tooltip;
-    endX = fiveLinkage.current_position(1) + L * cosd(phi);
-    endY = fiveLinkage.current_position(2) + L * sind(phi);
-    tooltip = [endX; endY];
+    if (fiveLinkage.isValid())
+        %update tooltip coordinate
+        
+        endX = fiveLinkage.P(1) + L * cosd(phi);
+        endY = fiveLinkage.P(2) + L * sind(phi);
+        tooltip = [endX; endY];
+    end
     
+    updateUI();
+end
+
+function updateUI()
+    drawActiveLink();
     drawPassiveLink();
     drawEndEffector();
     updateFkModeLabel();
 end
-        
+
 function results = drawActiveLink()
     global h_a1b1 h_a2b2;
     global fiveLinkage;
@@ -277,7 +298,7 @@ function results = drawActiveLink()
     results = 0;
 end
         
-function results = drawPassiveLink()
+function drawPassiveLink()
     global h_b1c1b2 h_b1c2b2;
     global fiveLinkage;
     global UIAxes;
@@ -289,30 +310,28 @@ function results = drawPassiveLink()
         delete(h_b1c2b2);
     end
     
-    if (fiveLinkage.fk_nSol < 1)
-        return;
-    end
-
-    if (fiveLinkage.current_configuration < 0)
+    if (~fiveLinkage.isValid())
         return;
     end
 
     upLineSpec  = '-mo';
     downLineSpec = ':mo';
-    if (fiveLinkage.current_configuration == 0)
+    
+    if (fiveLinkage.current_configuration == fiveLinkage.CONF_DOWN)
         upLineSpec  = ':mo';
         downLineSpec = '-mo';
     end
 
-    h_b1c1b2 = plot(UIAxes, [fiveLinkage.B1(1) fiveLinkage.fk_up(1) fiveLinkage.B2(1)] , [fiveLinkage.B1(2) fiveLinkage.fk_up(2) fiveLinkage.B2(2)], upLineSpec);
+    upP = fiveLinkage.fk_P(:, fiveLinkage.CONF_UP);
+    
+    h_b1c1b2 = plot(UIAxes, [fiveLinkage.B1(1) upP(1) fiveLinkage.B2(1)] , [fiveLinkage.B1(2) upP(2) fiveLinkage.B2(2)], upLineSpec);
     h_b1c1b2.PickableParts = 'none';
     
     if (fiveLinkage.fk_nSol == 2)
-        h_b1c2b2 = plot(UIAxes, [fiveLinkage.B1(1) fiveLinkage.fk_down(1) fiveLinkage.B2(1)] , [fiveLinkage.B1(2) fiveLinkage.fk_down(2) fiveLinkage.B2(2)], downLineSpec);
+        downP = fiveLinkage.fk_P(:, fiveLinkage.CONF_DOWN);
+        h_b1c2b2 = plot(UIAxes, [fiveLinkage.B1(1) downP(1) fiveLinkage.B2(1)] , [fiveLinkage.B1(2) downP(2) fiveLinkage.B2(2)], downLineSpec);
         h_b1c2b2.PickableParts = 'none';
-    end
-
-    results = 0;                    
+    end                   
 end
 
 function drawEndEffector()
@@ -329,11 +348,11 @@ function drawEndEffector()
         delete(h_end_effector);
     end
     
-    if (isempty(fiveLinkage.current_position))
+    if (~fiveLinkage.isValid())
         return;
     end
     
-    h_end_effector = plot(UIAxes, [fiveLinkage.current_position(1) tooltip(1)] , [fiveLinkage.current_position(2) tooltip(2)], '-cx');
+    h_end_effector = plot(UIAxes, [fiveLinkage.P(1) tooltip(1)] , [fiveLinkage.P(2) tooltip(2)], '-cx');
     h_end_effector.PickableParts = 'none';
 end
 
@@ -341,14 +360,14 @@ function  updateFkModeLabel()
     global UpLabel upModeLabel DownLabel downModeLabel UIModeKnob;
     global fiveLinkage;
 
-    upModeLabel.Text = fiveLinkage.getWorkMode(1);
-    downModeLabel.Text = fiveLinkage.getWorkMode(0);
+    upModeLabel.Text = fiveLinkage.getWorkModeString(fiveLinkage.CONF_UP);
+    downModeLabel.Text = fiveLinkage.getWorkModeString(fiveLinkage.CONF_DOWN);
 
-    if (fiveLinkage.current_configuration == 1)
+    if (fiveLinkage.current_configuration == fiveLinkage.CONF_UP)
         UpLabel.BackgroundColor = [0.00,1.00,0.00];
         DownLabel.BackgroundColor = 'none';
         UIModeKnob.Value = upModeLabel.Text;
-    elseif (fiveLinkage.current_configuration == 0)
+    elseif (fiveLinkage.current_configuration == fiveLinkage.CONF_DOWN)
         UpLabel.BackgroundColor = 'none';
         DownLabel.BackgroundColor = [0.00,1.00,0.00];
         UIModeKnob.Value = downModeLabel.Text;
