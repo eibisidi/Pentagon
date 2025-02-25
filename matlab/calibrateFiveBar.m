91b3841c510b4e499159759b12d1b0e88d48c7a1
eibisidi <5555735+eibisidi@user.noreply.gitee.com>
2025-02-25 13:03:05 +0800

    add simulation codes

--- matlab/calibrateFiveBar.m
+++ matlab/calibrateFiveBar.m
@@ -1,5 +1,6 @@
 syms L11 L12 L21 L22 D L23 GAMA DELTA1 DELTA2 X0 Y0 ALPHA;
 syms T1 T2;
+simulation = 1;
 skew = [ 0 -1; 1 0];
 rotation = [cos(GAMA) -sin(GAMA); sin(GAMA) cos(GAMA)];
 rOB1 = [L11 * cos(T1 + DELTA1) - D / 2; L11 * sin(T1 + DELTA1)];
@@ -18,11 +19,13 @@ yWF = pE(2);
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %                L11   L12  L21  L22   D   L23 GAMA DELTA1 DELTA2 X0 Y0 ALPHA
-vnom = [270; 370; 270; 370; 200; 370; 0; 0; 0; 0; 0; 0];
+%vnom = [270; 370; 270; 370; 200; 370; 0; 0; 0; 0; 0; 0];
 %vactual = vnom;
-vactual = vnom + [0.1; 0.07; 0.2; 0.11; 0.15; 3; deg2rad(4.1); deg2rad(1.5); deg2rad(2.7); 4; 7; deg2rad(3.5)];
+%vactual = vnom + [0.1; 0.07; 0.2; 0.11; 0.15; 3; deg2rad(4.1); deg2rad(1.5); deg2rad(2.7); 4; 7; deg2rad(3.5)];
+%vactual = vnom + [0.15; 0.21; 0.13; 0.23; 0.22; 7; deg2rad(4.1); deg2rad(6.5); deg2rad(2.7); 4; 7; deg2rad(3.5)];
 %vactual = vnom + [0.1; 0.3; 0.2; 0.4; 0.15; 3; deg2rad(4.1); 0; 0; 0 ; 0; 0];
 %vactual = vnom + [0.1; 0.3; 0.2; 0.4; 0.15; 3; deg2rad(4.1); deg2rad(1.5); deg2rad(2.7); 4; 7; deg2rad(3.5)];
+%Bad calibration configurations
 % means = [60; 348.617;
 %          40; 348.617;
 %          20; 348.617;
@@ -30,18 +33,21 @@ vactual = vnom + [0.1; 0.07; 0.2; 0.11; 0.15; 3; deg2rad(4.1); deg2rad(1.5); deg
 %          -20; 348.617;
 %          -40; 348.617;
 %          -60; 348.617;];
-means = [200; 348.617;
-         -200; 348.617;
-         310; 250
-         -310; 250;
-         150; 480;
-         -150; 480;
-         10; 470];
-est = zeros(size(means));
-n = size(means, 1) / 2;
-m = size(vnom, 1);
+
+means = [60; 348.617 + 10;
+         40; 348.617 ;
+         20; 348.617 + 10;
+         0; 348.617;
+         -20; 348.617 + 10;
+         -40; 348.617;
+         -60; 348.617 + 10;];
 
 phis = [];
+n = size(means, 1) / 2;
+if simulation == 1
+vnom = [270; 370; 270; 370; 200; 370; 0; 0; 0; 0; 0; 0];
+vactual = vnom + [0.15; 0.21; 0.13; 0.23; 0.22; 7; deg2rad(4.1); deg2rad(6.5); deg2rad(2.7); 4; 7; deg2rad(3.5)];
+
 for i = 1 : n
     position = [means(2*i - 1);means(2*i)];
     phi = ik_sym(position, vnom);
@@ -50,6 +56,18 @@ for i = 1 : n
     means(2*i-1) = xtmp;
     means(2*i) = ytmp;
 end
+else
+for i = 1 : n
+    position = [means(2*i - 1);means(2*i)];
+    phi = ik_sym(position, vnom);
+    phis = [phis, phi];
+
+end    
+end
+
+
+est = zeros(size(means));
+m = size(vnom, 1);
 
 Jk_formula = [diff(xWF, L11), diff(xWF, L12), diff(xWF, L21),  diff(xWF, L22), diff(xWF, D), diff(xWF, L23), diff(xWF, GAMA), diff(xWF, DELTA1), diff(xWF, DELTA2), diff(xWF, X0), diff(xWF, Y0), diff(xWF, ALPHA);
     diff(yWF, L11), diff(yWF, L12), diff(yWF, L21),  diff(yWF, L22), diff(yWF, D), diff(yWF, L23), diff(yWF, GAMA), diff(yWF, DELTA1), diff(yWF, DELTA2), diff(yWF, X0), diff(yWF, Y0), diff(yWF, ALPHA)];
